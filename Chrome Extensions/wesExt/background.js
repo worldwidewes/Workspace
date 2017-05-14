8364cb785a41e1af8528caa8104aed6e25394634
Wes <wlimtiaco@gmail.com>
2017-05-14 10:34:11 -0700

    Updates

--- Chrome Extensions/wesExt/background.js
+++ Chrome Extensions/wesExt/background.js
@@ -1,7 +1,9 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
-  });
-});
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   // No tabs or host permissions needed!
+//   console.log('Turning ' + tab.url + ' red!');
+//   chrome.tabs.executeScript({
+//     code: 'document.body.style.backgroundColor="red"'
+//   });
+// });
+
+console.log("the background.js file is working");
