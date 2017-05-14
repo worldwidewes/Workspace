6c0243f077c8599b1fa000baad16ccc9aefb5542
Wes <wlimtiaco@gmail.com>
2017-05-14 13:58:49 -0700

    Updates

--- Chrome Extensions/wesExt/myscript.js
+++ Chrome Extensions/wesExt/myscript.js
@@ -1,19 +1,28 @@
-window.onload = function(){
-	//Add jQuery to page
-	if(1){
-		(function () {
+    (function () {
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js';
+        s.src = '//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     })();
-    	console.log("jquery 3.2.1 loaded");
-	}
+
+    window.wcl = {};
+    //wcl tools
+    wcl.alert = function(msg){
+        /*FIRE AN ALERT MESSAGE*/
+        alert(msg);
+    };
+    wcl.jsloop = function(selector){
+        //Run jQuery Loop
+        jQuery(selector).each(function(i,elem){
+            if(elem.text){
+                console.log(elem.text);
+            }
+        })
+    }
+
+    //Confirmation of Loaded
 	console.log("myscript.js ran to the end!");
-
-
-
 
 }
